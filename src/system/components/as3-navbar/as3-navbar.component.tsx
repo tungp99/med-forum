3ee0fe88c68acf277f23d5bddfa6d62b0d596db6
Bao Pham Gia <bao.phamgia@hotmail.com>
2022-03-24 22:36:37 +0700

    Supreme Icon Created BY BaoPham

--- src/system/components/as3-navbar/as3-navbar.component.tsx
+++ src/system/components/as3-navbar/as3-navbar.component.tsx
@@ -1,4 +1,4 @@
-import { Container, Image, Navbar, Stack } from 'react-bootstrap'
+import { Container, Navbar, Stack } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import {
   mdiAccountCircle,
@@ -40,12 +40,11 @@ export function AS3Navbar() {
         <Container
           fluid
           className="py-1 justify-content-between">
-          <Image
-            className="me-5"
-            src="https://via.placeholder.com/80x36.jpg"
-            fluid
+          <div
             onClick={() => navigate('/')}
-          />
+            className="supremeIcon fs-4 ms-1">
+            AS3
+          </div>
 
           <AS3Input
             className="as3-navbar-search"
