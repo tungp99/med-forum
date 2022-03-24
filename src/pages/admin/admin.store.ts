702acd62d2eebab44757161c835ca9faf0e30145
Bao Pham Gia <bao.phamgia@hotmail.com>
2022-03-24 16:29:21 +0700

    ?

--- src/pages/admin/admin.store.ts
+++ src/pages/admin/admin.store.ts
@@ -1,18 +1,14 @@
 import { DateTime } from 'luxon'
 import { StoreAction } from 'system/store'
 
-type Store = { filterType: string; filterTime: string; editable: boolean }
+type Store = { filterType: string; filterTime: string }
 const initialState: Store = {
   filterTime: `${DateTime.now().startOf('day').toISO()}`,
   filterType: 'Today',
-  editable: false,
 }
 export const adminStore = (
   state = initialState,
-  action: StoreAction<
-    'FILTER_POSTS_UPDATE' | 'AUTH_EDITABLE' | 'UNAUTH_EDITABLE',
-    string
-  >
+  action: StoreAction<'FILTER_POSTS_UPDATE', string>
 ): Store => {
   switch (action.type) {
     case 'FILTER_POSTS_UPDATE':
@@ -31,10 +27,6 @@ export const adminStore = (
         filterType: action.payload as string,
         filterTime: filterTime,
       }
-    case 'AUTH_EDITABLE':
-      return { ...state, editable: true }
-    case 'UNAUTH_EDITABLE':
-      return { ...state, editable: false }
     default:
       return state
   }
